eaef9a525f42a8ffa620972fea589fa56da5eedf
KamogeloMashike98 <82131449+Kamogelo98@users.noreply.github.com>
2023-05-25 12:59:16 +0200

    styling changes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
+import Weather from './Weather';
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
      <h1>Weather App</h1>
+     <Weather />
      <footer>
       <a href="https://github.com/Kamogelo98/react-weather-app" target="_blank" rel="noreferrer" >Open Source Project </a>
         coded by Kamogelo Mashike</footer>
