4dc7af82fe51761b2c185843aa13363277a8c3fd
KamogeloMashike98 <82131449+Kamogelo98@users.noreply.github.com>
2023-05-25 10:10:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
-      
+     <footer>
+      <a href="https://github.com/Kamogelo98/react-weather-app" target="_blank" rel="noreferrer" >Open Source Project </a>
+        coded by Kamogelo Mashike</footer>
     </div>
   );
 }
-
+ 
 export default App;
