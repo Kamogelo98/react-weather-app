45211fa9dcd1a46bcc96c38edcbebe94609f7955
KamogeloMashike98 <82131449+Kamogelo98@users.noreply.github.com>
2023-05-25 10:36:56 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='container'>
      <h1>Weather App</h1>
      <footer>
       <a href="https://github.com/Kamogelo98/react-weather-app" target="_blank" rel="noreferrer" >Open Source Project </a>
         coded by Kamogelo Mashike</footer>
+        </div>
     </div>
   );
 }
