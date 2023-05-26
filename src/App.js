6daa8bd337694708080ea28b87ae9966170cf86f
KamogeloMashike98 <82131449+Kamogelo98@users.noreply.github.com>
2023-05-26 15:24:21 +0200

    weather icon updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div className='container'>
      <h1>Weather App</h1>
-     <Weather />
+     <Weather defaultCity="Cape Town"/>
      <footer>
       <a href="https://github.com/Kamogelo98/react-weather-app" target="_blank" rel="noreferrer" >Open Source Project </a>
         coded by Kamogelo Mashike</footer>
--- src/Weather.js
+++ src/Weather.js
@@ -1,43 +1,67 @@
-import React from "react";
+import React ,{useState} from "react";
+import axios from "axios";
+import WeatherInfo from "./WeatherInfo";
 import "./Weather.css";
 
 
 
-export default function Weather(){
-    return(
+export default function Weather(props){
+
+    const [weatherData,setWeatherData]= useState({ready:false});
+    const [city,setCity]= useState(props.defaultCity);
+
+    function handleResponse(response){
+        setWeatherData({
+            ready: true,
+            temperature: response.data.main.temp,
+            humidity: response.data.main.humidity,
+            date:new Date(response.data.dt*1000),
+            description: response.data.weather[0].description,
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+            wind: response.data.wind.speed,
+            city: response.data.name
+        });
+        
+    }
+
+function search(){
+let apiKey = `cd173a006b0e51dac58c6d8064c94178`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+   axios.get(apiUrl).then(handleResponse);
+}    
+
+function handleSubmit(event){
+    event.preventDefault();
+    search();
+}
+
+function handleCityChange(event){
+  setCity(event.target.value);
+}
+
+    if(weatherData.ready){
+
+         return(
  <div className="Weather">
-    <form>
+    <form onSubmit={handleSubmit}>
         <div className="row">
         <div className="col-9">
-        <input type="search" placeholder="Search for a City..." className="form-control" autoFocus="on"/>
+        <input type="search" placeholder="Search for a City..." className="form-control" autoFocus="on" onChange={handleCityChange}/>
         </div>
         <div className="col-3">
         <input type="submit" value="Search" className="btn btn-primary w-100"/>
         </div>
         </div>
     </form>
-    <h1>Pretoria</h1>
-    <ul>
-        <li>Thursday 11:00</li>
-        <li>Mostly Cloudy</li>
-    </ul>
-    <div className="row">
-        <div className="col-6">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy"/>
-         <span className="temperature">6</span>
-         <span className="unit">°C</span>
-        </div>
-        <div className="col-6">
-            <ul>
-                <li>Precipitation: 18%</li>
-                <li>Humidity: 67%</li>
-                <li>Wind: 14km/h</li>
-            </ul>
 
-        </div>
-    </div>
+    <WeatherInfo data={weatherData} />
+    </div> 
 
- </div>
+);
 
-    ) 
+    }else{
+          search();   
+          return "Loading ..."
+    }
 }
