261b5c1dead1472f771194a1733ed6503bbc4123
KamogeloMashike98 <82131449+Kamogelo98@users.noreply.github.com>
2023-05-30 10:53:31 +0200

    weather forecast final fixes added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className='container'>
-     <h1>Weather App</h1>
+     <h1 className="text-center">React Weather App.</h1>
      <Weather defaultCity="Cape Town"/>
      <footer>
       <a href="https://github.com/Kamogelo98/react-weather-app" target="_blank" rel="noreferrer" >Open Source Project </a>
--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,18 +6,44 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded,setLoaded] = useState(false);
   let [forecast,setForecast] = useState(null);
+
+//If coordinates change
+//set loaded to false fixes the focast
+  useEffect(()=>{
+    setLoaded(false);
+  },[props.coordinates]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
+
+
   if(loaded){
      return (
     <div className="WeatherForecast">
       <div className="row">
+        
         <div className="col">
           <WeatherForecastDay data={forecast[0]} />
           </div>
+           <div className="col">
+          <WeatherForecastDay data={forecast[1]} />
+          </div>
+           <div className="col">
+          <WeatherForecastDay data={forecast[2]} />
+          </div>
+           <div className="col">
+          <WeatherForecastDay data={forecast[3]} />
+          </div>
+           <div className="col">
+          <WeatherForecastDay data={forecast[4]} />
+          </div>
+           <div className="col">
+          <WeatherForecastDay data={forecast[5]} />
+          </div>
+
         </div>
       </div>
     
